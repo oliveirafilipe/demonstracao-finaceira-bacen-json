ef8ab1e7f7a83bb8075742f2c449b0d8c84205d4
Filipe Oliveira <ooliveirafilipe@gmail.com>
2023-05-03 09:26:45 -0300

    refactor: add github link in header

--- header.go
+++ header.go
@@ -1,3 +1,5 @@
 package main
 
-var header string = "Demonstrativos Financeiros - Converte CSV para JSON\nVersão: v1 - Autor: @oliveirafilipe\n"
+var header string = "Demonstrativos Financeiros - Converte CSV para JSON\n\n" +
+	"Para instruções de uso acesse: https://github.com/oliveirafilipe/demonstracao-finaceira-bacen-json\n" +
+	"Versão: v1 - Autor: @oliveirafilipe\n"
