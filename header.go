9214147cdfd171bef6cf57039d6420ac72161088
Filipe Oliveira <ooliveirafilipe@gmail.com>
2023-05-24 17:39:14 -0300

    feat: release workflow

--- header.go
+++ header.go
@@ -1,5 +1,16 @@
 package main
 
-var header string = "Demonstrativos Financeiros - Converte CSV para JSON\n\n" +
-	"Para instruções de uso acesse: https://github.com/oliveirafilipe/demonstracao-finaceira-bacen-json\n" +
-	"Versão: v1 - Autor: @oliveirafilipe\n"
+import "github.com/oliveirafilipe/demonstracao-finaceira-bacen-json/metadata"
+
+func getHeader() string {
+
+	var header string = "Demonstrativos Financeiros BACEN - Converte CSV para JSON\n" +
+		"(Versão: " + metadata.Version + " - Autor: @oliveirafilipe)\n\n" +
+		"Para instruções de uso, ou novas versões, acesse: https://github.com/oliveirafilipe/demonstracao-finaceira-bacen-json\n"
+
+	if metadata.Version == "development" {
+		header = header + "==> Você provavelmente não deveria estar usando essa versão.\n"
+	}
+
+	return header
+}
